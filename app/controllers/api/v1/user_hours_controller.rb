0699db47f5dbcccc7315474386c7337bcf9e5f19
CarsonRey <c.rey494@gmail.com>
2019-02-06 22:11:48 -0500

    creation of association between User and Hour is limited to 7 associations per user, updates dynamically with user input

--- app/controllers/api/v1/user_hours_controller.rb
+++ app/controllers/api/v1/user_hours_controller.rb
@@ -7,7 +7,21 @@ class Api::V1::UserHoursController < ApplicationController
 
 
   def create
+    # byebug
     @user_hour = UserHour.new(user_hour_params)
+
+    @user = User.find(params[:user_id])
+    @hour = Hour.find(params[:hour_id])
+
+        @hours = @user.hours.select do |hourSet|
+          hourSet.day_id != @hour.day_id
+        end
+
+    @user.hours = @hours
+
+    # we want to go through the hours that the user does have and "remove" the hour that is being updated
+    # AKA we're seeing if the current user_hour.hour.day_id matches any of the hour.day_ids in the user's collection
+
     if @user_hour.save
       render json: @user_hour
     end
